062fd58948e6c06f1e563135109a5de8f87298c8
billxu <billxu0521@gmail.com>
2022-07-08 15:56:14 +0800

    no message

---
+++ js/78.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkbotw_app"]=self["webpackChunkbotw_app"]||[]).push([[78],{78:function(t,e,n){n.r(e),n.d(e,{startStatusTap:function(){return a}});var o=n(65),r=n(487),s=n(587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const a=()=>{const t=window;t.addEventListener("statusTap",(()=>{(0,o.wj)((()=>{const e=t.innerWidth,n=t.innerHeight,a=document.elementFromPoint(e/2,n/2);if(!a)return;const i=(0,r.a)(a);i&&new Promise((t=>(0,s.c)(i,t))).then((()=>{(0,o.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,r.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78.js.map
